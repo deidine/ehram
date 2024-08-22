a0d3505fec0456e9f6b93893532cfc73fb7629c8
deidinevectormind <deidine.cheigeur@vector-mind.com>
2024-08-22 09:17:18 +0000

    ui

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,8 +1,9 @@
+import Link from 'next/link';
 import React from 'react';
 
 export default function Page() {
   return (
-    <div className="flex items-center justify-center w-full h-screen bg-gray-100">
+    <div className="flex flex-col gap-4 items-center justify-center w-full h-screen bg-gray-100">
       <div className="text-center px-8 py-4 bg-white shadow-md rounded-md">
         <h1 className="text-4xl text-blue-700 font-bold mb-4">
           الموقع قيد البناء
@@ -11,6 +12,9 @@ export default function Page() {
           نشكر صبركم ونتمنى أن نلبي تطلعاتكم في موقع شباب أشرم.
         </p>
       </div>
+      <Link href="./home/profile" className="text-lg text-white bg-green-500 hover:bg-green-600 px-6 py-2 rounded-lg">
+    الملف الشخصي
+      </Link>
     </div>
   );
 }
