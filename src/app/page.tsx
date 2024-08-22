658832781342653fa71447fa29e2c827f4d99878
deidinevectormind <deidine.cheigeur@vector-mind.com>
2024-08-22 14:02:14 +0000

    deiden

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,8 @@ export default function Home() {
       <div className="mb-8">
           <Image src="/logo2.jpg" width={400} height={100} alt="Logo" />
         </div>
-        <h1 className="text-4xl text-wrap font-bold text-green-700 mb-8">
-        أهلا بكم في موقع شباب أشرم1 للبناء
+        <h1 className="text-4xl text-center text-wrap font-bold text-green-700 mb-8">
+        أهلا بكم في موقع نادي شباب أشرم1 للبناء
       </h1>
       <div className="space-x-4">
         <Link href="/register" className="text-lg text-white bg-blue-500 hover:bg-blue-600 px-6 py-2 rounded-lg">
