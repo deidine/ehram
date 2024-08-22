26268c2758a4a10fcba8431449f5b9f9aaf7ac23
Malik-Talebely <soudoud2000@gmail.com>
2024-08-22 18:15:04 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import Image from "next/image";
+import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex flex-col items-center  gap-4 min-h-screen bg-gray-100">
